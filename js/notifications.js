3a69a8ea46a5eace3d9a1a1a33b62c403c92e296
Doug Sherk <github@sherk.me>
2015-10-21 19:53:38 -0700

    Initial notifications code

--- js/notifications.js
+++ js/notifications.js
@@ -1,35 +1,61 @@
-if (navigator.push) {
+function sendPushUrl(data) {
+  var url = data.url;
+
+  var getUuid = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      var r = window.crypto.getRandomValues(new Uint32Array(1))[0] % 16,
+          v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+
+  var sendEndpoint = function(endpoint) {
+    $.ajax({
+      method: 'POST',
+      url: url,
+      data: {
+        uuid: uuid,
+        endpoint: endpoint
+      }
+    });
+  };
+
+  var uuid = localStorage.getItem('uuid') || getUuid();
+
   var req = navigator.push.register();
 
   req.onsuccess = function(e) {
     var endpoint = req.result;
-
-    console.log('*****');
-    console.log(endpoint);
+    sendEndpoint(endpoint);
   };
 
   req.onerror = function(e) {
     console.error("Error getting a new endpoint: " + JSON.stringify(e));
   };
 
-  console.log('setting push handler');
   navigator.mozSetMessageHandler('push', function(e) {
     console.log('My endpoint is ' + e.pushEndpoint);
     console.log('My new version is ' +  e.version);
   });
 
-  console.log('setting push-register handler');
   navigator.mozSetMessageHandler('push-register', function(e) {
-    console.log('push-register received, I need to register my endpoint(s) again!');
-
     var req = navigator.push.register();
     req.onsuccess = function(e) {
       var endpoint = req.result;
       console.log("New endpoint: " + endpoint );
+      sendEndpoint(endpoint);
     }
 
     req.onerror = function(e) {
       console.error("Error getting a new endpoint: " + JSON.stringify(e));
     }
+  });
+}
+
+if (navigator.push) {
+  $.ajax({
+    url: 'push-server.json',
+    success: sendPushUrl,
+    dataType: 'json'
   });
 }
